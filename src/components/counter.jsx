fcb5700128d2beebce85233ca257dfb5d5ccc21b
Moira Leon <moira.leon2@gmail.com>
2022-02-24 21:53:47 -0700

    adding styles to html

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,10 +5,18 @@ class Counter extends Component {
     state={
         count:0,
     };
+
+    // styles ={
+    //     fontSize: 10, //automatically appends px
+    //     fontWeight: 'bold',
+
+    // }
+    //can use styles tag like so{/* style={this.styles} or do in line style style={{fontSize:30}} */} 
+
     render() { 
         return (
         <React.Fragment>
-            <span className='badge badge-primary m-2'>{this.formatCount()}</span>
+            <span className='badge badge-primary m-2'>{this.formatCount()}</span> 
             <button className='btn btn-secondary btn-sm'>Increment</button>
         </React.Fragment>
         );
