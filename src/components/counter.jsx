7edc24938c583f97a5443f09313a88e05b6874c9
Moira Leon <moira.leon2@gmail.com>
2022-02-24 21:40:59 -0700

    adding bootstrap

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,14 +4,12 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state={
         count:0,
-        imageUrl: 'https://picsum.photos/200'
     };
     render() { 
         return (
         <React.Fragment>
-            <img src={this.state.imageUrl} ></img>
-            <span>{this.formatCount()}</span>
-            <button>Increment</button>
+            <span className='badge badge-primary m-2'>{this.formatCount()}</span>
+            <button className='btn btn-secondary btn-sm'>Increment</button>
         </React.Fragment>
         );
     }
